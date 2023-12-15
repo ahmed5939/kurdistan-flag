03167972ca2b3f3c8279e870b19d7b05c549bf0e
ahmed <smmahmad04@gmail.com>
2023-12-15 23:31:51 +0300

    logic done

--- app/game/page.jsx
+++ app/game/page.jsx
@@ -11,24 +11,24 @@ const Page = () => {
     const [selectedOptionId, setSelectedOptionId] = useState('');
 
 
-    useEffect(()  =>  {
+    useEffect(() => {
         // Fetch the questions from the API
         fetch('/api')
             .then((response) => response.json())
             .then((data) => {
                 const shuffledQuestions = shuffleArray(data.data);
                 setQuestions(shuffledQuestions);
-    
+
             });
-           
+
     }, []);
 
     const shuffleArray = (array) => {
-        for (let i = array.length - 1; i > 0; i--) { 
-            const j = Math.floor(Math.random() * (i + 1)); 
-            [array[i], array[j]] = [array[j], array[i]]; 
-          } 
-            return array;
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
     };
 
 
@@ -46,7 +46,7 @@ const Page = () => {
             if (newUnlockedParts < questions.length) {
                 setCurrentQuestionIndex(currentQuestionIndex + 1); // Go to next question
             }
-            
+
             setSelectedOptionId('');
         } else {
             alert("Incorrect answer. Try again!");
@@ -55,80 +55,83 @@ const Page = () => {
 
     // Calculate the percentage of the flag to reveal
     const partsToReveal = unlockedParts;
-     return (
+    return (
+
         <div className="flex flex-col items-center justify-center min-h-screen bg-lime-100">
             <div className=' w-2/3 h-11/12'>
-            <h1 className="text-4xl font-bold mb-4">Kurdistan Flag Quiz</h1>
+                <h1 className="text-4xl font-bold mb-4">Kurdistan Flag Quiz</h1>
 
-          <div className="relative mt-8 w-1/2 h-96 rounded border-2 border-yellow-700">
-            
-            <Image
-                src={krdImage}
-                alt="Kurdistan Flag"
-                layout="fill"
-                objectFit="cover"
-                className=""
-                
-            />
+                <div className="relative mt-8 w-1/2 h-96 rounded border-2 border-yellow-700">
 
-           
-            
-            <div
-                className="absolute top-0 left-0 w-full h-full"
-                style={{
-                    display: 'grid',
-                    gridTemplateColumns: `repeat(3, 1fr)`,
-                    gridTemplateRows: 'repeat(3, 1fr)',
-                }}
-            >
-               {Array.from({ length: 9 }).map((_, index) => (
-                    <div
-                        key={index}
-                        style={{
-                            backgroundColor: index < partsToReveal ? 'transparent' : 'rgba(128, 128, 128, 0.9)',
-                        }}
+                    <Image
+                        src={krdImage}
+                        alt="Kurdistan Flag"
+                        layout="fill"
+                        objectFit="cover"
+                        className=""
+
                     />
-                ))}
 
-                
 
-                
-            </div>
-        </div>
 
-            {unlockedParts === 9 && <div className="mt-4 text-green-600">Congratulations! You've unlocked the entire flag!</div>}
-          
-            {questions.length > 0 && unlockedParts<9?(
-                <>
-                    <div className="mb-4 text-xl font-bold">{questions[currentQuestionIndex]?.text}</div>
-
-                    <div className="mb-4">
-                        {questions[currentQuestionIndex]?.options.map((option) => (
-                            <button
-                                key={option.id}
-                                className={`py-2 px-4 rounded m-1 ${option.id === selectedOptionId ? ' bg-amber-500 hover:bg-green-700 text-white' : 'bg-blue-500 hover:bg-blue-700 text-white'}`}
-                                onClick={() => handleOptionSelect(option.id)}
-                            >
-                                {option.text}
-                            </button>
-                        ))}
-                    </div>
-
-                    <button
-                        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                        onClick={handleSubmitAnswer}
+                    <div
+                        className="absolute top-0 left-0 w-full h-full"
+                        style={{
+                            display: 'grid',
+                            gridTemplateColumns: `repeat(3, 1fr)`,
+                            gridTemplateRows: 'repeat(3, 1fr)',
+                        }}
                     >
-                        Submit Answer
-                    </button>
-                </>
+                        {Array.from({ length: 9 }).map((_, index) => (
+                            <div
+                                key={index}
+                                style={{
+                                    backgroundColor: index < partsToReveal ? 'transparent' : 'rgba(128, 128, 128, 0.9)',
+                                }}
+                            />
+                        ))}
 
-                
-            ) : (
-                <div></div>
-            )}
+
+
+
+                    </div>
+                </div>
+
+                {unlockedParts === 9 && <div className="mt-4 text-green-600">Congratulations! You've unlocked the entire flag!</div>}
+
+                {questions.length > 0 && unlockedParts < 9 ? (
+                    <>
+                        <div className="mb-4 text-xl font-bold">{questions[currentQuestionIndex]?.text}</div>
+
+                        <div className="mb-4">
+                            {questions[currentQuestionIndex]?.options.map((option) => (
+                                <button
+                                    key={option.id}
+                                    className={`py-2 px-4 rounded m-1 ${option.id === selectedOptionId ? ' bg-amber-500 hover:bg-green-700 text-white' : 'bg-blue-500 hover:bg-blue-700 text-white'}`}
+                                    onClick={() => handleOptionSelect(option.id)}
+                                >
+                                    {option.text}
+                                </button>
+                            ))}
+                        </div>
+
+                        <button
+                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                            onClick={handleSubmitAnswer}
+                        >
+                            Submit Answer
+                        </button>
+                    </>
+
+
+                ) : (
+                    <div></div>
+                )}
             </div>
         </div>
+
     );
+
 };
 
 export default Page;
--- app/page.js
+++ app/page.js
@@ -2,14 +2,14 @@ import React from "react";
 import Link from "next/link";
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2" style={{backgroundImage: "url('/home/ahmed/kurdistan-flag/public/Flag_of_Kurdistan.png')"}}>
       <h1 className="text-4xl font-bold mb-5">Welcome to "Unlock the Flag"!</h1>
       <p className="text-lg mb-2">Join us in celebrating Kurdistan National Flag Day.</p>
       <p className="text-lg mb-2">Learn about Kurdish culture, history, and geography through engaging questions.</p>
       <p className="text-lg mb-2">Each correct answer reveals a part of the Kurdistan flag.</p>
       <p className="text-lg mb-5">Are you ready to unlock the flag?</p>
       <Link href="/game">
-        <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">Start</button>
+        <button className="px-4 py-2 font-bold text-wite bg-blue-500 rounded hover:bg-blue-700 animate-pulse">Start</button>
       </Link>
     </div>
   );
